2cf3ab64fd711028b181d8ec50dafc52b3ec6adb
lreiter <lorenz.r@gmx.at>
2018-12-04 21:41:19 +0100

    new begininx

--- R_skripte/data_collection.R
+++ R_skripte/data_collection.R
@@ -6,7 +6,7 @@ EurostatTOC <- get_eurostat_toc()
 GDP_Nuts2 <- get_eurostat('nama_10r_2gdp', time_format = "raw")
 GDP_Nuts3 <- get_eurostat('nama_10r_3gdp', time_format = "raw")
 Pop_Nuts2 <- get_eurostat('demo_r_pjangroup', time_format = "raw")
-Pop_Nuts3 <- get_eurostat('demo_r_pjanaggr3', time_format = "raw")
+Pop_Nuts3 <- get_eurostat('demo_r_pjanaggr3', time_format = "raw", stringsAsFactors = FALSE)
 
 
 GDP_Nuts2$time <- as.numeric(GDP_Nuts2$time)
@@ -15,9 +15,7 @@ Pop_Nuts2$time <- as.numeric(Pop_Nuts2$time)
 Pop_Nuts3$time <- as.numeric(Pop_Nuts3$time)
 
 
-Pop_Nuts3_new <- Pop_Nuts3 %>% mutate(NUTS=nchar(lapply(Pop_Nuts3$geo, toString))) %>% filter(time >= 2000, sex=="T", age=="TOTAL", NUTS==5)
-Pop_Nuts3_new$geo<-droplevels(Pop_Nuts3_new$geo)
+Pop_Nuts3_new <- Pop_Nuts3 %>% filter(time >= 2000, sex=="T", age=="TOTAL", nchar(geo)==5)
 
-table(Pop_Nuts3_new$time)
                                    
 substr(x, start = 1, stop = 2)                                   
