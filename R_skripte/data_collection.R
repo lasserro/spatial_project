13f636463cdd5aecc0b4158a52bdb06ee7a434ba
lasserro <robert-lasser@hotmail.com>
2018-12-04 19:17:45 +0100

    first dataselection eurostat

--- R_skripte/data_collection.R
+++ R_skripte/data_collection.R
@@ -1,2 +1,10 @@
 library(eurostat)
 EurostatTOC <- get_eurostat_toc()
+
+df <- get_eurostat(c('nama_10r_2gdp', 'nama_10r_3gdp', 'demo_r_pjangroup', 
+                   'demo_r_pjangrp3'), time_format = "raw")
+
+GDP_Nuts2 <- get_eurostat('nama_10r_2gdp', time_format = "raw")
+GDP_Nuts3 <- get_eurostat('nama_10r_3gdp', time_format = "raw")
+Pop_Nuts2 <- get_eurostat('demo_r_pjangroup', time_format = "raw")
+Pop_Nuts3 <- get_eurostat('demo_r_pjangrp3', time_format = "raw")
