fae81bd83eefd4192f77e2d886f2f8378947c170
lreiter <lorenz.r@gmx.at>
2018-12-11 17:38:36 +0100

    started shaping

--- R_skripte/combining shapefiles with data.R
+++ R_skripte/combining shapefiles with data.R
@@ -35,3 +35,38 @@ shp3 <- readOGR(dsn = "./data", layer ="NUTS_RG_03M_2013_4326_LEVL_3")
 #overseas <- c("FRA1", "FRA2", "FRA3", "FRA4", "FRZZ", "FRA5", "PT20", "PT30", "PTZZ", "ES70", "ESZZ")
 # we can also exclude all oversea territories
 #shp <- shp[! shp$NUTS_ID %in% overseas, ]
+
+
+shp2x <- merge(shp2, nuts_2, all.x = FALSE, all.y = TRUE, by.x = "NUTS_ID", by.y = "geo_2")
+
+
+nuts_2x <-nuts_2 %>% group_by(time) %>% merge(., shp2, all.y = FALSE, all.x = TRUE, by.y = "NUTS_ID", by.x = "geo_2")
+
+
+
+
+nuts_2 <- nuts_2 %>% filter(time==2010)
+
+shp2x <- shp2[ shp$CNTR_CODE %in% com, ]
+
+nonEU<-list("AL","CH","EF","EU","IS","ME","MK","NO","TR","LI")
+drop<-c(nonEU,"DK","DE","FR","PL")
+
+
+unique(shp2$CNTR_CODE)
+unique(nuts_2$country)
+
+com <-intersect(unique(shp2$CNTR_CODE),
+          unique(nuts_2$country))
+
+
+overseas <- c("FRA1", "FRA2", "FRA3", "FRA4", "FRZZ", "FRA5", "PT20", "PT30", "PTZZ", "ES70", "ESZZ")
+
+# we can also exclude all oversea territories
+shp <- shp[! shp$NUTS_ID %in% overseas, ]
+
+
+
+
+
+
