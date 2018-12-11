3b1307f42c213743e41ddf8dbbd2e1ca1d8a9fc2
lreiter <lorenz.r@gmx.at>
2018-12-12 00:50:54 +0100

    weniger ordner

--- R_skripte/Data/data_collection_clean.R
+++ R_skripte/Data/data_collection_clean.R
@@ -1,27 +1,50 @@
 library(eurostat)
 library(dplyr)
 
-###############################DEFINE#########################
-##Which countries to drop
+if(set==max){
+############################## DEFINE ########################
+## Which countries to drop
 nonEU<-list("AL","CH","EF","EU","IS","ME","MK","NO","TR","LI")
-drop<-c(nonEU,"DK")
-##the time frame
-period<-c(2003:2016)
-# müssen wahrscheinlich 2016, wenn nicht auch 2015 droppen. siehe (table(gdp3$time))
+drop<-nonEU
+## the time frame
+period<-c(2000:2016)
+# müssen wahrscheinlich 2016, wenn nicht auch 2015 droppen.
+# siehe (table(gdp3$time))
 
-##unit of GDP-measurement
+## unit of GDP-measurement
 measure="EUR_HAB"
 ##############################################################
+}
+if(set==min){
+  ###############################DEFINE#########################
+  nonEU<-list("AL","CH","EF","EU","IS","ME","MK","NO","TR","LI")
+  drop<-c(nonEU,"DK","DE","FR","PL")
+  period<-c(2004:2014)
+  measure="EUR_HAB"
+  ############################################################## 
+} 
+  
+############## Download ########################################################
 
+suppressWarnings(                                 
+  try(load("./RData/GDP_Nuts3"),silent = TRUE))   ##try suppresses errors
+  
+suppressWarnings(
+  try(load("./RData/Pop_Nuts3"),silent = TRUE))
+
+if(exists(GDP_Nuts3)){
 GDP_Nuts3 <- get_eurostat('nama_10r_3gdp', time_format = "raw",
                           stringsAsFactors = FALSE)
 Pop_Nuts3 <- get_eurostat('demo_r_pjanaggr3', time_format = "raw",
                           stringsAsFactors = FALSE)
 
-##Data Transformation
+save(cn,file = "./RData/GDP_Nuts3")
+save(cn,file = "./RData/Pop_Nuts3")
+}
+
+## Data Transformation
+
 #1. Population: Nuts3
-
-
 pop3 <- Pop_Nuts3 %>% mutate(country=substr(geo, start = 1, stop = 2)) %>%
                       filter(nchar(geo)==5,
                              sex=="T",
