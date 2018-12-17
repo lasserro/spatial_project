78c8d54bc5ce426b2ba5b085f8dc62a300ca1193
lasserro <robert-lasser@hotmail.com>
2018-12-17 15:40:18 +0100

    included overseas filter in the transformation script

--- R_skripte/Data_Transformation/data_transformation_ERD.R
+++ R_skripte/Data_Transformation/data_transformation_ERD.R
@@ -29,10 +29,10 @@ if(min==1){
 charcols <- c("nuts_level", "country", "nuts_2", "nuts_code")
 
 POP_ERD <- POP_ERD %>% select(charcols, paste(period)) %>%
-  filter(!country %in% drop | !nuts_2 %in% overseas) 
+  filter(!country %in% drop)  %>% filter(!nuts_2 %in% overseas) 
 
 GDP_ERD <- GDP_ERD %>% select(charcols, paste(period)) %>%
-  filter(!country %in% drop | !nuts_2 %in% overseas)
+  filter(!country %in% drop ) %>% filter(!nuts_2 %in% overseas)
 
 ############# ACHUTNG, der folgende code schmeisst NUTS 3 regionen raus, die 
 # nicht in pop3 und gdp3 übereinstimmen (vom namen her), das betrifft 6 Stück.
