d550d5e121f4b0d967f27456b7853112aa46a403
lasserro <robert-lasser@hotmail.com>
2019-01-21 18:30:36 +0100

    minor changes

--- R_skripte/tables_plots.R
+++ R_skripte/tables_plots.R
@@ -1,15 +1,17 @@
+table_ols_list <- list()
 
 table1 <- matrix(NA,13,3)
-table1[,1] <- c('Constant (alpha)', 'GDP per capita (beta)', 
+rownames(table1) <- c('Constant (alpha)', 'GDP per capita (beta)', 
                 '(GDP per capita )^2 (gamma)', 'NUTS 3 Number (lambda)', 
                 '(Coefficient of determination (R^2)', 'Jarque-Bera (normality)',
                 'Breusch-Pagan (heteroskedasticity)', 'Diagnostics for spatial dependence',
                 'Moran', 'LMERR', 'R-LMERR', 'LMLAG', 'R-LMLAG')
 
+
 #### access coefficients
 summaries <- lapply(lm, summary)
 # ...coefficents with p values:
-lapply(summaries[1], function(x) x$coefficients[, c(1,4)])
+#lapply(summaries[1], function(x) x$coefficients[, c(1,4)])
 summary1 <- lapply(summaries[1], function(x) x$coefficients)[[1]]
 
 # ...or r-squared values
@@ -30,5 +32,11 @@ table1[11,2:3] <- lm_tests[1,5:6]
 table1[12,2:3] <- lm_tests[1,3:4]
 table1[13,2:3] <- lm_tests[1,7:8]
 
-View(table1)
+table1 <- round(table1, digits = 4)
 
+
+table_ols_list <- list()
+table <- matrix(1,13,3)
+for (i in 1:k) {
+  table_ols_list[i] <- table
+    }
