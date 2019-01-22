b147d2c0fe0d70691592c3eeca4fdf04c921cd84
lasserro <robert-lasser@hotmail.com>
2019-01-22 15:28:33 +0100

    replace w.list.k with w.list

--- R_skripte/Spatial Tests.R
+++ R_skripte/Spatial Tests.R
@@ -18,10 +18,10 @@ for (i in 1:k) {
 moran_Y <- matrix(ncol = 4, nrow = k)
 dimnames(moran_Y) <- list(period,c("Moran's I", "Moran_p.value","Moran.mc","Moran.mc_p.value"))
 for (i in 1:k) {
-  f<- moran.test(shp_list[[i]]$Y, listw = W.list.k, alternative = "greater", randomisation = FALSE)
+  f<- moran.test(shp_list[[i]]$Y, listw = W.list, alternative = "greater", randomisation = FALSE)
   moran_Y[i,1] <- f$estimate[1]
   moran_Y[i,2] <- f$p.value
-  f<- moran.mc(shp_list[[i]]$Y, listw = W.list.k, alternative = "greater", nsim = 100)
+  f<- moran.mc(shp_list[[i]]$Y, listw = W.list, alternative = "greater", nsim = 100)
   moran_Y[i,3] <- f$statistic
   moran_Y[i,4] <- f$p.value
 }
@@ -31,13 +31,13 @@ moran_res <- matrix(ncol = 6, nrow = k)
 dimnames(moran_res) <- list(period,c("Moran's I", "Moran-p-value","Moran.mc","Moran.mc_p.value","Geary C","Geary_p.value"))
 
 for (i in 1:k) {
-  f <- moran.test(lm[[i]]$residuals, listw = W.list.k, alternative = "greater", randomisation = FALSE)
+  f <- moran.test(lm[[i]]$residuals, listw = W.list, alternative = "greater", randomisation = FALSE)
   moran_res[i,1] <- f$estimate[1]
   moran_res[i,2] <- f$p.value
-  f <- moran.mc(lm[[i]]$residuals, listw = W.list.k, alternative = "greater", nsim = 100)
+  f <- moran.mc(lm[[i]]$residuals, listw = W.list, alternative = "greater", nsim = 100)
   moran_res[i,3] <- f$statistic
   moran_res[i,4] <- f$p.value
-  f <- geary.test(lm[[i]]$residuals, listw = W.list.k, alternative = "greater")
+  f <- geary.test(lm[[i]]$residuals, listw = W.list, alternative = "greater")
   moran_res[i,5] <- f$estimate[1]
   moran_res[i,6] <- f$p.value
 }
@@ -48,7 +48,7 @@ lisa.test <- list()
 #lisa.test <- matrix(ncol = 6, nrow = n_2)
 #dimnames(lisa.test) <- list(period,c("LISA","Lisa_p.value"))
 for (i in 1:k) {
-  lisa.test[[i]] <- localmoran(lm[[k]]$residuals, listw = W.list.k, alternative = "greater")
+  lisa.test[[i]] <- localmoran(lm[[k]]$residuals, listw = W.list, alternative = "greater")
 }
 
 ## and attempt to plot lisa
@@ -83,8 +83,8 @@ for (i in 1:k) {
   #Passt das für uns? was sind indivicual effects?
   
   for (i in tests) {
-    local.rob.LM[1, i] <- slmtest(f1, data = data.long, listw = W.list.k, test = i)$statistic
-    local.rob.LM[2, i] <- slmtest(f1, data = data.long, listw = W.list.k, test = i)$p.value
+    local.rob.LM[1, i] <- slmtest(f1, data = data.long, listw = W.list, test = i)$statistic
+    local.rob.LM[2, i] <- slmtest(f1, data = data.long, listw = W.list, test = i)$p.value
   }
   round(local.rob.LM, digits = 4)
   # sign. values for rlml and rlme mean that we are likely dealing with spatial dependence in the lag and in the error term, as well
